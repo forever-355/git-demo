81471b426886a0c575a9c23a8dc0be5e120dd2b7
${forever-355} <${953814328@qq.com}>
2020-11-15 17:16:08 +0800

    有了MainTest2类后的提交

--- Demo-Module-1/src/com/git/MainTest2.java
+++ Demo-Module-1/src/com/git/MainTest2.java
@@ -2,6 +2,6 @@ package com.git;
 
 public class MainTest2 {
     public static void main(String[] args) {
-        System.out.println("hello git -- ");
+        System.out.println("hello git-----");
     }
 }
