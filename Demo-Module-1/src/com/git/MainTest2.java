5d2234e334ad2ee50fa4f493bfe2c87c7cbdc095
${forever-355} <${953814328@qq.com}>
2020-11-15 17:27:27 +0800

    MainTest2类更新，提交

--- Demo-Module-1/src/com/git/MainTest2.java
+++ Demo-Module-1/src/com/git/MainTest2.java
@@ -3,5 +3,6 @@ package com.git;
 public class MainTest2 {
     public static void main(String[] args) {
         System.out.println("hello git-----");
+        System.out.println("第二行：hello git");
     }
 }
